cfd053aaca50af1f787acc89c8f54602615ec516
ismaelrsousa <ismaelsousa136@hotmail.com>
2021-09-21 20:10:02 -0300

    Hello World

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
+      <Text>Coming Soon: Learn Better APP</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +19,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
